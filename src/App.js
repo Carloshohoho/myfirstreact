a3e5a09b92cb1d7bfb346ec8dbd3bbda8fd05b97
Carloshohoho <90942261+Carloshohoho@users.noreply.github.com>
2021-11-10 13:29:25 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. hehehehe
-        </p>
+        <p>hi friends</p>
+        <p>-Carlos</p>
+        <div>
+        <h1>
+            "Hello"
+            "Peter"
+            "a.k.a"
+            "Spiderman"
+            </h1>
+            </div>
         <a
           className="App-link"
           href="https://reactjs.org"
